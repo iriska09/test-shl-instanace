851285b4ae6b2124dbd1ff50c24fcbf0e9e28fe6
iriska09 <kudaibergenovayryska09@gmail.com>
2025-02-06 12:36:48 -0600

    Initial commit for instance-create

--- main.tf
+++ main.tf
@@ -11,3 +11,33 @@ resource "aws_instance" "example" {
     Environment = "production"
   }
 }
+
+resource "aws_s3_bucket" "example" {
+  bucket = "my-example-bucket"
+
+  tags = {
+    Name        = "example-bucket"
+    Environment = "production"
+  }
+
+  versioning {
+    enabled = true
+  }
+
+  server_side_encryption_configuration {
+    rule {
+      apply_server_side_encryption_by_default {
+        sse_algorithm = "AES256"
+      }
+    }
+  }
+}
+
+resource "aws_s3_bucket_public_access_block" "example" {
+  bucket = aws_s3_bucket.example.id
+
+  block_public_acls   = true
+  block_public_policy = true
+  ignore_public_acls  = true
+  restrict_public_buckets = true
+}
