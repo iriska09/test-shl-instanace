eb9f87fbe8cd80b77805da11f5aa94f3a0e3142a
iriska09 <kudaibergenovayryska09@gmail.com>
2025-02-06 18:44:56 -0600

    changed jenkinsfile

--- backend.tf
+++ backend.tf
@@ -1,9 +1,9 @@
-terraform {
-  backend "s3" {
-    bucket = "terraform-session-backend-yrys"
-    key    = "shared-lib-test/terraform.tfstate" //Path to your Remote Backend File (terraform.tfstate)
-    region = "us-west-2"
-    dynamodb_table = "terraform-lock-file"
-    encrypt = true
-  }
-}
+# terraform {
+#   backend "s3" {
+#     bucket = "terraform-session-backend-yrys"
+#     key    = "shared-lib-test/terraform.tfstate" //Path to your Remote Backend File (terraform.tfstate)
+#     region = "us-west-2"
+#     dynamodb_table = "terraform-lock-file"
+#     encrypt = true
+#   }
+# }
