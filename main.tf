54451dbfbced3a0818336ce2b4881658ec30d2ab
iriska09 <kudaibergenovayryska09@gmail.com>
2025-02-08 15:28:40 -0600

    changed jenkinsfile

--- main.tf
+++ main.tf
@@ -3,12 +3,12 @@ resource "aws_instance" "example" {
   instance_type = "t2.micro"
   ebs_optimized = false
 
-  # IAM instance profile is not attached
-  # iam_instance_profile = aws_iam_instance_profile.example.name
+  #IAM instance profile is not attached
+  iam_instance_profile = aws_iam_instance_profile.example.name
 
-  # No tags provided
-  # tags = {
-  #   Name        = "example-instance"
-  #   Environment = "test"
-  # }
+  #No tags provided
+  tags = {
+    Name        = "example-instance"
+    Environment = "test"
+  }
 }
